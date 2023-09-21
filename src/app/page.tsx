1bdff436d6e86c50b1dec33ca2cadd541eb2ecba
hundehausen <mail@grischadaum.de>
2023-09-21 22:50:32 +0200

    add x, sethforprivacy, responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,27 +15,74 @@ import {
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 import { FaGithub } from "react-icons/fa";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   const { colorScheme, setColorScheme } = useMantineColorScheme();
   const { services, stateFunctions } = useServices();
 
   return (
-    <AppShell header={{ height: 48 }}>
+    <AppShell
+      header={{
+        height: {
+          base: 96,
+          xs: 80,
+        },
+      }}
+    >
       <AppShell.Header
         styles={{
           header: {
-            padding: "0 32px",
+            padding: "0 24px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
           },
         }}
       >
-        <Flex justify="space-between" align="center">
+        <Flex justify="space-between" align="center" w={"100%"}>
           <Title order={1}>Monero Suite</Title>
-          <Flex align="center" justify="space-between" gap="md">
+          <Flex
+            align="center"
+            justify="space-between"
+            gap={{
+              base: 12,
+              xs: 16,
+            }}
+          >
+            <Link
+              href="https://x.com/hundehausen/"
+              title="hundehausen's profile on x.com"
+              target="_blank"
+              style={{ color: "inherit", textDecoration: "none", height: 32 }}
+            >
+              <Image
+                src="/hundehausen.png"
+                alt="hundehausen's profile on x.com"
+                style={{ borderRadius: "50%" }}
+                width={32}
+                height={32}
+              />
+            </Link>
+            <Link
+              href="https://sethforprivacy.com/guides/run-a-p2pool-node/"
+              target="_blank"
+              title="Monero related guides from sethforprivacy.com"
+              style={{ color: "inherit", textDecoration: "none", height: 32 }}
+            >
+              <Image
+                src="/seth.png"
+                alt="Monero related guides from sethforprivacy.com"
+                style={{ borderRadius: "50%" }}
+                width={32}
+                height={32}
+              />
+            </Link>
             <Link
               href="https://github.com/hundehausen/monero-suite"
               target="_blank"
               rel="noopener noreferrer"
+              title="Source code on GitHub"
               style={{
                 color: "inherit",
                 textDecoration: "none",
@@ -54,14 +101,8 @@ export default function Home() {
           </Flex>
         </Flex>
       </AppShell.Header>
-      <AppShell.Main>
-        <Container
-          styles={{
-            root: {
-              padding: "32px 0",
-            },
-          }}
-        >
+      <AppShell.Main mt={48}>
+        <Container>
           <Grid
             gutter="lg"
             styles={{
